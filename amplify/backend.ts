82f53845250cb2220500bcfa5e28c30c266d0a08
Cesarmorfron <cesar.morillo@dormakaba.com>
2024-09-16 16:28:22 +0200

    function log streams

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,11 +4,50 @@ import { data } from './data/resource';
 import { storage } from './storage/resource';
 import { myFirstFunction } from './my-first-function/resource';
 import { sayHello } from './functions/say-hello/resources';
+import { myDynamoDBFunction } from "./functions/dynamoDB-function/resource";
+import { Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
+import { Stack } from "aws-cdk-lib";
+import { StartingPosition, EventSourceMapping } from "aws-cdk-lib/aws-lambda";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
   myFirstFunction,
-  sayHello
+  sayHello,
+  myDynamoDBFunction,
 });
+
+
+const todoTable = backend.data.resources.tables["Contact"];
+const policy = new Policy(
+  Stack.of(todoTable),
+  "MyDynamoDBFunctionStreamingPolicy",
+  {
+    statements: [
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "dynamodb:DescribeStream",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:ListStreams",
+        ],
+        resources: ["*"],
+      }),
+    ],
+  }
+);
+backend.myDynamoDBFunction.resources.lambda.role?.attachInlinePolicy(policy);
+
+const mapping = new EventSourceMapping(
+  Stack.of(todoTable),
+  "MyDynamoDBFunctionTodoEventStreamMapping",
+  {
+    target: backend.myDynamoDBFunction.resources.lambda,
+    eventSourceArn: todoTable.tableStreamArn,
+    startingPosition: StartingPosition.LATEST,
+  }
+);
+
+mapping.node.addDependency(policy);
