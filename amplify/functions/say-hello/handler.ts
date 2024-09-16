c11942c125d21334ccbb092af37481d4034ebc2a
Cesarmorfron <cesar.morillo@dormakaba.com>
2024-09-16 16:39:03 +0200

    say hello simple

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,41 +1,10 @@
-import { Amplify } from 'aws-amplify';
-import type { Schema } from "../../data/resource";
-import outputs from '../../../amplify_outputs.json';
-
-// Configurar Amplify
-Amplify.configure(outputs);
-
-// Generar el cliente con el esquema de Amplify
-import { generateClient } from 'aws-amplify/data';
-const client = generateClient<Schema>();
+import type { Schema } from "../../data/resource"
 
 export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
-  // Extraer idUser desde los argumentos
-  const { idUser } = event.arguments;
-  console.log('idUser:', idUser);
-
-  const fetchContacts = async () => {
-    try {
-      const { data: contacts, errors } = await client.models.Contact.list({
-        filter: {
-          idUser: {
-            eq: idUser!,
-          },
-        },
-      });
-
-      if (errors) {
-        console.error(errors);
-      } else {
-        console.log('contacts:', contacts);
-      }
-    } catch (error) {
-      console.error('Error fetching contacts:', error);
-    }
-  };
-
-  await fetchContacts();
-
-  // Devolver respuesta
-  return `Hello, ${idUser}!`;
-};
+   // arguments typed from `.arguments()`
+   const { idUser } = event.arguments
+   console.log('name')
+   console.log(idUser)
+   // return typed from `.returns()`
+   return `Hello, ${idUser}!`
+}
