5c19c4ba986ac64fc8bb357257382e6ba557131e
Cesarmorfron <cesar.morillo@dormakaba.com>
2024-09-16 15:20:38 +0200

    use generateClient

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,34 +1,38 @@
 import type { Schema } from "../../data/resource"
-import AWS from 'aws-sdk';
+import { generateClient } from 'aws-amplify/data';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const client = generateClient<Schema>();
 
 export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
- // Extraer el idUser desde los argumentos
- const { idUser } = event.arguments;
- console.log('idUser:', idUser);
- 
- // Parámetros de escaneo completo de la tabla
- const params = {
-   TableName: 'Contact-xlznjcoayzddxlockvuufrw5vi-NONE',
- };
+   // arguments typed from `.arguments()`
+   const { idUser } = event.arguments
+   console.log('name')
+   console.log(idUser)
 
- try {
-   // Ejecutar el escaneo completo de la tabla
-   const data = await dynamoDB.scan(params).promise();
-   console.log('Escaneo exitoso:', data);
-   
-   // Filtrar los resultados por idUser
-  //  const filteredItems = data.Items.filter(item => item.idUser === idUser);
-   
-   // Retornar los elementos filtrados
-  //  return filteredItems;
-  return `Hello, ${idUser}!`
+   const fetchContacts = async () => {
+    try {
+      const { data: contacts, errors } = await client.models.Contact.list({
+        filter: {
+          idUser: {
+            eq: idUser,
+          },
+        },
+      });
 
- } catch (err) {
-   console.error('Error al escanear DynamoDB:', err);
-   
-   // Retornar un error en caso de fallo
-   throw new Error('Error al escanear la tabla Contact');
- }
+      if (errors) {
+        console.error(errors);
+      }
+      else {
+        console.log('contacts')
+        console.log(contacts)
+      }
+    } catch (error) {
+      console.error('Error fetching contacts:', error);
+    }
+  };
+
+  fetchContacts();
+
+   // return typed from `.returns()`
+   return `Hello, ${idUser}!`
 }
