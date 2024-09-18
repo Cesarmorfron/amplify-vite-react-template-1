25b1daadfa6eb075f7feba26930d8f19176b2526
Cesarmorfron <cesar.morillo@dormakaba.com>
2024-09-18 12:15:40 +0200

    lambda confirm s3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,12 +1,16 @@
-import { defineStorage } from '@aws-amplify/backend';
+import { defineFunction, defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
-
   access: (allow) => ({
     'csvs-contacts/*': [
       allow.authenticated.to(['read', 'write']),
       allow.guest.to(['read', 'write']),
     ],
   }),
+  triggers: {
+    onUpload: defineFunction({
+      entry: './on-upload-handler.ts'
+    })
+  }
 });
