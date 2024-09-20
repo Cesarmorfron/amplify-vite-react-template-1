819791888f9f17a6ce977e10a34af45fa7a83133
Cesarmorfron <cesar.morillo@dormakaba.com>
2024-09-20 16:57:17 +0200

    logs

--- amplify/functions/blacklist-lambda/handler.ts
+++ amplify/functions/blacklist-lambda/handler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Schema } from '../../data/resource';
 import { DynamoDB } from 'aws-sdk';
 
@@ -10,25 +11,11 @@ export const handler: Schema['blacklistLambda']['functionHandler'] = async (
   event
 ) => {
   console.log(event);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const eventObject = event as any;
-
-  // Obtener los registros del evento
-  const record = eventObject.Records[0].ses;
-  console.log(record);
-
-  // Obtener la dirección del remitente
-  const from = record.mail.commonHeaders.from[0];
-  console.log(`Email received from: ${from}`);
-
-  const email = from.match(/<([^>]+)>/)?.[1];
-
-  if (!email) {
-    console.log('No se encontró ningún email.');
-    return 'no email valid';
-  }
+  const email = getEmailFromEvent(event);
 
   const whitelist = await getWhitelistContact(email);
+  console.log('whitelist.Item')
+  console.log(whitelist.Item)
 
   if (!whitelist.Item) return 'no email whitelisted';
 
@@ -41,6 +28,7 @@ export const handler: Schema['blacklistLambda']['functionHandler'] = async (
 
   const contactsToDelete = await queryContactsToDelete(email);
 
+  console.log('contactsToDelete')
   console.log(contactsToDelete)
   if (!contactsToDelete.Items) return 'no contacts to delete';
 
@@ -51,6 +39,24 @@ export const handler: Schema['blacklistLambda']['functionHandler'] = async (
 
   return 'success';
 };
+
+function getEmailFromEvent(event:any) {
+    const eventObject = event as any;
+
+    const record = eventObject.Records[0].ses;
+    console.log(record);
+  
+    const from = record.mail.commonHeaders.from[0];
+    console.log(`Email received from: ${from}`);
+  
+    const email = from.match(/<([^>]+)>/)?.[1];
+  
+    if (!email) {
+      console.log('No se encontró ningún email.');
+      throw new Error('no email valid')
+    }
+    return email
+}
 
 async function queryContactsToDelete(email: string) {
   return await dynamoDb
@@ -65,23 +71,27 @@ async function queryContactsToDelete(email: string) {
     .promise();
 }
 
-async function deleteWhitelistContact(email: string) {
-  await dynamoDb
-    .delete({
-      TableName: TABLE_NAME_WHITELIST,
-      Key: {
-        id: email,
-      },
-    })
-    .promise();
-}
-
 async function deleteContact(id: string) {
+    console.log('id')
+    console.log(id)
   await dynamoDb
     .delete({
       TableName: TABLE_NAME_CONTACT,
       Key: {
         id: id,
+      },
+    })
+    .promise();
+}
+
+async function deleteWhitelistContact(email: string) {
+    console.log('email')
+    console.log(email)
+  await dynamoDb
+    .delete({
+      TableName: TABLE_NAME_WHITELIST,
+      Key: {
+        id: email,
       },
     })
     .promise();
@@ -103,7 +113,7 @@ async function createBlacklistContact(isoDate: string, email: string) {
     .put({
       TableName: TABLE_NAME_BLACKLIST,
       Item: {
-        __typename: 'WhitelistContact',
+        __typename: 'BlacklistContact',
         createdAt: isoDate,
         updatedAt: isoDate,
         id: email,
