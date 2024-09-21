4019675fd3a29b44b8d74798c9a59d607b164ca7
Cesar <cemofron@gmail.com>
2024-09-21 18:10:57 +0200

    useeffect

--- src/components/CrudTable.tsx
+++ src/components/CrudTable.tsx
@@ -62,16 +62,16 @@ const CrudTable: React.FC<CrudTableProps> = ({ onRowClick }) => {
   };
 
   useEffect(() => {
-    // const token = getCognitoToken();
-    // if (token) {
-    //   const decodedToken = jwtDecode(token);
-    //   const companyValue = (decodedToken as any)['custom:company'];
-    //   console.log(company);
-    //   setCompany(companyValue);
-    // }
-    // fetchByCompany();
+    const token = getCognitoToken();
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const companyValue = (decodedToken as any)['custom:company'];
+      console.log(company);
+      setCompany(companyValue);
+    }
+    fetchByCompany();
 
-    const user = {name: 'name',lastName: 'name',city: 'name',birthDate: 'name',email: 'name', deceased: true, vigil: 'hola', funeral: 'funeral', dateDeceased: 'dateDeceased' };setItems([user, user, user, user, user]);
+    // const user = {name: 'name',lastName: 'name',city: 'name',birthDate: 'name',email: 'name', deceased: true, vigil: 'hola', funeral: 'funeral', dateDeceased: 'dateDeceased' };setItems([user, user, user, user, user]);
 
     // const sub = client.models.User.observeQuery().subscribe({
     //   next: ({ items }) => {
